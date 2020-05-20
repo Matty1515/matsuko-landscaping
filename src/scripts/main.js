9421824393cf5abe78379f7742fd63fa43626ab2
Matthew Shaw <imhullu15@gmail.com>
2020-05-20 16:13:32 +0100

    started gsap animations

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,105 @@
 /* jshint esversion: 6 */
-
 window.addEventListener('load', function () {
 
-console.log("Javascript is working!");
+
+// === GSAP & ScrollMagic Variables ===
+const Controller = new ScrollMagic.Controller();
+const IntroTl = new TimelineMax({});
+const ProbioticsTl = new TimelineMax({});
+const JumbotronTopTl = new TimelineMax({});
+const JumbotronBottTl = new TimelineMax({});
+const FaqTl = new TimelineMax({});
+const AboutUsTl = new TimelineMax({});
+const EnquireTl = new TimelineMax({});
+
+
+// === Intro Animation ===
+IntroTl
+.staggerFrom('.intro-text > *', 1, {
+	opacity: 0,
+	y: 20,
+	stagger: 0.2
+});
+
+const IntroScene = new ScrollMagic.Scene({
+	triggerElement: ".intro"
+}).setTween(IntroTl).addTo(Controller);
+
+
+// === Probiotics Animation ===
+ProbioticsTl
+.from('.probiotics-center', 1, {
+	opacity: 0,
+	y: 20
+})
+.from('.probiotics-left', 1, {
+	opacity: 0,
+	x: 20,
+	y: 20
+}, '-=0.5')
+.from('.probiotics-right', 1, {
+	opacity: 0,
+	x: -20,
+	y: 20
+}, '-=1');
+
+const ProbioticsScene = new ScrollMagic.Scene({
+	triggerElement: ".probiotics"
+}).setTween(ProbioticsTl).addTo(Controller);
+
+
+// === Jumbotron Top Animation ===
+JumbotronTopTl
+.from('.probiotics-bottom p', 1, {
+	y: 20,
+	opacity: 0
+});
+
+const JumbotronTopScene = new ScrollMagic.Scene({
+	triggerElement: ".jumbotop-trigger"
+}).setTween(JumbotronTopTl).addTo(Controller);
+
+
+// === Jumbotron Bottom Animation ===
+JumbotronBottTl
+.from('.faq-top p', 1, {
+	y: -20,
+	opacity: 0
+});
+
+const JumbotronBottScene = new ScrollMagic.Scene({
+	triggerElement: ".faq-top"
+}).setTween(JumbotronBottTl).addTo(Controller);
+
+
+// === Faq Animation ===
+
+
+
+// === About Us Animation ===
+AboutUsTl
+.staggerFrom('.aboutus-right > *', 1, {
+	opacity: 0,
+	y: 20,
+	stagger: 0.2
+});
+
+const AboutUsScene = new ScrollMagic.Scene({
+	triggerElement: ".aboutus"
+}).setTween(AboutUsTl).addTo(Controller);
+
+
+// === Enquire Animation ===
+EnquireTl
+.from('.enquire-left button', 1, {
+	opacity: 0,
+	scale: 0.8,
+	y: 20
+});
+
+const EnquireScene = new ScrollMagic.Scene({
+	triggerElement: ".enquire"
+}).setTween(EnquireTl).addTo(Controller);
+
 
 });
